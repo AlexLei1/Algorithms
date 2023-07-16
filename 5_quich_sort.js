990664ce244dfe72d4ced2d3e68953f30f25c854
Alex Lei <sasha.volkov2015@yandex.ru>
2023-07-16 22:03:30 +0300

    parsed tasks with arrays

--- 5_quich_sort.js
+++ 5_quich_sort.js
@@ -2,26 +2,19 @@
 const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,]
 let count = 0
 
-function quickSort(array) {
-    if (array.length <= 1) {
-        return array
-    }
-    let pivotIndex = Math.floor(array.length / 2);
-    let pivot = array[pivotIndex]
-    let less = []
-    let greater = []
-    for (let i = 0; i < array.length; i++) {
-        count += 1
-        if(i === pivotIndex)
-            continue
-        if (array[i] < pivot) {
-            less.push(array[i])
-        } else {
-            greater.push(array[i])
-        }
-    }
-    return [...quickSort(less), pivot, ...quickSort(greater)]
-}
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  var pivot = arr[0];
+  
+  var left = []; 
+  var right = [];
+  for (var i = 1; i < arr.length; i++) {
+    arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+  }
+  return quicksort(left).concat(pivot, quicksort(right));
+};
 
-console.log(quickSort(arr))
-console.log('count', count) // O(n)
+console.log(quicksort(arr))
+
