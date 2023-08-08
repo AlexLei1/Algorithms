e3469a77acc67a143c8d7ff28d1920c27eda83a0
Alex Lei <sasha.volkov2015@yandex.ru>
2023-08-08 13:54:41 +0300

    solved several algorithms

--- tasks/tasks_arr.js
+++ tasks/tasks_arr.js
@@ -246,29 +246,29 @@ console.log(productExceptSelf([-1,1,0,-3,3])) // [0,0,9,0,0]
 var findDuplicate = function(nums) { // Arrays O(N)
 	var seen = new Set()
 	for(var i = 0; i < nums.length; i++){
-			//*если в колекции уже есть число возвращаем его || добавляем в колекцию
-			if(seen.has(nums[i])){ 
-					return nums[i]  
-			}else{
-					seen.add(nums[i]) 
-			}
+		//*если в колекции уже есть число возвращаем его || добавляем в колекцию
+		if(seen.has(nums[i])){ 
+			return nums[i]  
+		}else{
+			seen.add(nums[i]) 
+		}
 	}
 };
 
 console.log(findDuplicate([3,1,3,4,2])) // 3
 console.log(findDuplicate([1,3,4,2,2])) // 2
 
-//TODO-------------------------------------------
+//!-------------------------------------------
 
 var findDuplicate = function(nums) {
 	const arrayOfElements = new Array(nums.length).fill(0);
 
 	for (let i = 0; i < nums.length; i++) {
-			const numValue = nums[i];
-			arrayOfElements[numValue]++;
-			if (arrayOfElements[numValue] == 2) {
-					return numValue;
-			}
+		const numValue = nums[i];
+		arrayOfElements[numValue]++;
+		if (arrayOfElements[numValue] == 2) {
+				return numValue;
+		}
 	}
 
 	return undefined;
@@ -277,7 +277,7 @@ var findDuplicate = function(nums) {
 console.log(findDuplicate([3,1,3,4,2])) // 3
 console.log(findDuplicate([1,3,4,2,2])) // 2
 
-//TODO--------------------------------------------------------------------------------------------------------------------
+//!--------------------------------------------------------------------------------------------------------------------
 
 var findDuplicate = function(nums) { // Binary Search O(N)
     
@@ -312,29 +312,32 @@ var findDuplicate = function(nums) { // Binary Search O(N)
 console.log(findDuplicate([3,1,3,4,2])) // 3
 console.log(findDuplicate([1,3,4,2,2])) // 2
 
-//TODO--------------------------------------------------------------------------------------------------------------------
+//!--------------------------------------------------------------------------------------------------------------------
 
 function findDuplicate(nums) { // Two Pointers O(N)
-	let slow = nums[0]
-	let fast = nums[nums[0]]
+	let slow = nums[0] 
+	let fast = nums[nums[0]] 
 
 	while(slow != fast) {
-			slow = nums[slow]
-			fast = nums[nums[fast]]
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		console.log(slow, fast)
 	}
 
 	slow = 0
 
 	while(slow != fast) {
-			slow = nums[slow]
-			fast = nums[fast]
+		slow = nums[slow]
+		fast = nums[fast]
+		console.log(slow, fast)
 	}
 
 	return slow
 };
 
-console.log(findDuplicate([3,1,3,4,2])) // 3
 console.log(findDuplicate([1,3,4,2,2])) // 2
+console.log(findDuplicate([3,1,3,4,2])) // 3
+
 
 //!====================================================================================================================
 
@@ -345,6 +348,213 @@ console.log(findDuplicate([1,3,4,2,2])) // 2
 //todo Вы должны написать алгоритм, который работает за время O(n) и использует только постоянное дополнительное пространство.
 //todo (Arrays)
 
+var findDuplicates = function(nums) { // Arrays O(N)
+	// инициализируем массив result
+	let result = []
+	// инициализируем колекцию 
+	let set = new Set()
+	// запускаем цикл по nums
+	for(let i = 0; i <= nums.length; i++) {
+		// если в колекции есть num добавляем в массив || добавляем в колекцию
+		if(set.has(nums[i])) {
+			result.push(nums[i])
+		} else {
+			set.add(nums[i])
+		}
+	}
+	return result
+};
 
+console.log(findDuplicates([4,3,2,7,8,2,3,1])) // [2,3]
+console.log(findDuplicates([1,1,2])) // [1]
+console.log(findDuplicates([1])) // []
 
+//!====================================================================================================================
 
+//* 73. Set Matrix Zeroes
+
+//todo Если задана целочисленная матричная матрица m x n, если элемент равен 0, установите всю ее строку и столбец в 0.
+//todo Вы должны сделать это на месте.
+//todo (Arrays) 
+
+var setZeroes = function(matrix) { // HashTable, Matrix //! 0(N?)
+	// соклько столбцов в матрице
+    let y = matrix.length
+	// соклько элементов в столбце матрицы
+	let x = matrix[0].length
+
+	let map = new Map() // {[key: 1, value: 1]}
+	let set = new Set()
+	for(let i = 0; i < y; i++){
+		for(let j = 0; j < x; j++) {
+
+			if (matrix[i][j] === 0) {
+				map.set(i, j)
+				set.add(j)
+			}
+		}
+	}
+
+	for(let i = 0; i < y; i++){
+		if (map.has(i)) {
+			matrix[i] = new Array(x).fill(0)
+		}
+		for(let j = 0; j < x; j++) {
+
+			if (set.has(j)) {
+				matrix[i][j] = 0
+				console.log(matrix[i][j] = 0)
+			}
+
+		}
+	}
+	
+	return matrix
+};
+
+console.log(setZeroes([[1,1,1],[1,0,1],[1,1,1]])) // [[1,0,1],[0,0,0],[1,0,1]]
+console.log(setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]])) // [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+//!--------------------------------------------------------------------------------------------------------------------
+
+var setZeroes = function(matrix) { // Array, HashTable, Matrix
+    let zerosRow = new Set(), zerosCol = new Set() 
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j] === 0) {
+                zerosRow.add(i)
+                zerosCol.add(j)
+            }
+        }
+    }
+    zerosRow = [...zerosRow]
+    zerosCol = [...zerosCol]
+
+    for(let i = 0; i < zerosRow.length; i++) {
+        for(let j = 0; j < matrix[0].length; j++) {
+            matrix[zerosRow[i]][j] = 0 
+        }
+    }
+    for(let i = 0; i < zerosCol.length; i++) {
+        for(let j = 0; j < matrix.length; j++) {
+            matrix[j][zerosCol[i]] = 0 
+        }
+    }
+};
+
+console.log(setZeroes([[1,1,1],[1,0,1],[1,1,1]])) // [[1,0,1],[0,0,0],[1,0,1]]
+console.log(setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]])) // [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+//!====================================================================================================================
+
+//* 54. Spiral Matrix
+
+//todo Учитывая матрицу m x n, вернуть все элементы матрицы в порядке спирали.
+//todo (Arrays) 
+
+var spiralOrder = function(matrix) { // Arrays
+
+	// количество столбцов в матрице
+    let yLength = matrix.length
+	// количество элементов в столбце
+	let xLength = matrix[0].length
+	// столбцы
+    let y = 0 
+	// элемнты столбцов
+	let x = 0
+	// счетчик
+	let count = 0
+	// количество элементов в матрице
+	let area = xLength * yLength
+	// массив с результатом
+	let result = []
+
+	// выполняем цикл пока счетчик меньше количеста элемнтов в матрице
+    while(count < area){
+		// цикл работает при условии что счетчик < количества элементов в матрице && i < количества элементов в столбце - x
+        for(let i = x; count < area && i < xLength - x; i++) {
+			result.push(matrix[y][i])
+			count++;
+		}
+        y++;
+        for(let i = y; count < area && i < yLength -y +1; i++) {
+            result.push(matrix[i][(xLength-1) - x])
+            count++;
+        }
+        x++;
+        for(let i = (xLength-1) - x; count < area && i >= x-1; i--) {
+            result.push(matrix[(yLength-1) - (y-1)][i])
+            count++;
+        }
+
+        for(let i = (yLength-1) - y; count<area && i >= y; i--) {
+            result.push(matrix[i][x-1])
+            count++;
+        }
+
+    }
+    return result
+};
+
+console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]])) // [1,2,3,6,9,8,7,4,5]
+console.log(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]])) // [1,2,3,4,8,12,11,10,9,5,6,7]
+
+//!====================================================================================================================
+
+//* 48. Rotate Image
+
+//todo Вам дана двумерная матрица размера n x n, представляющая изображение, поверните изображение на 90 градусов (по часовой стрелке). 
+//todo Вам нужно повернуть изображение на месте, что означает, что вы должны напрямую изменить входную 2D-матрицу.
+//todo НЕ выделяйте другую 2D-матрицу и не выполняйте поворот.
+//todo (Arrays) 
+
+var rotate = function(matrix) { // Arrays
+
+    for(let i=0;i<matrix.length;i++){
+        for(let j=i;j<matrix.length;j++){
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for(let i=0;i<matrix.length;i++){
+        for(let j=0;j<matrix.length/2;j++){
+            let temp=matrix[i][j];
+            matrix[i][j]=matrix[i][matrix.length-1-j];
+            matrix[i][matrix.length-1-j]=temp;
+        }
+    }
+    return matrix;
+
+};
+
+console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]])) // [[7,4,1],[8,5,2],[9,6,3]]
+console.log(spiralOrder([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])) // [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+//!====================================================================================================================
+
+//* 128. Longest Consecutive Sequence
+
+//todo Учитывая несортированный массив целых чисел nums, вернуть длину самой длинной последовательности последовательных элементов. 
+//todo Вы должны написать алгоритм, работающий за время O(n).
+//todo (Arrays) 
+
+var longestConsecutive = function(nums) {
+    nums.sort((a,b) => {a - b})
+};
+
+console.log(longestConsecutive([100,4,200,1,3,2])) // 4
+console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1])) // 9
+
+//!====================================================================================================================
+
+//* 41. First Missing Positive
+
+//todo Учитывая несортированный целочисленный массив nums, вернуть наименьшее пропущенное положительное целое число.
+//todo Вы должны реализовать алгоритм, который выполняется за время O(n) и использует вспомогательное пространство O(1).
+//todo (Arrays) 
+
+var firstMissingPositive = function(nums) {
+    
+};
